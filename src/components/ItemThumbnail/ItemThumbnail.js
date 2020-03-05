6b976a802876331a9636d7f9adc2dbe0aeba9047
André Martins <andre@basedigital.pt>
2020-03-05 11:15:06 +0000

    Add link to item detail

--- src/components/ItemThumbnail/ItemThumbnail.js
+++ src/components/ItemThumbnail/ItemThumbnail.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { ItemThumbnailStyled } from './ItemThumbnailStyled';
+import { Link } from 'react-router-dom';
 
 export const ItemThumbnail = ({ itemId, title, voteAverage, poster }) => {
-  voteAverage = voteAverage + '';
-  const dec = voteAverage.split('.')[1];
-  const len = dec && dec.length > 1 ? dec.length : 1;
-  const vote = Number(voteAverage).toFixed(len);
+  voteAverage = voteAverage.toFixed(1);
 
   return (
-    <ItemThumbnailStyled>
-      <div className="score">{vote}</div>
-      <img src={poster} alt={title} />
-      <h1>{title}</h1>
-    </ItemThumbnailStyled>
+    <Link to={`/${itemId}`}>
+      <ItemThumbnailStyled>
+        <div className="score">{voteAverage}</div>
+        <img src={poster} alt={title} />
+        <h1>{title}</h1>
+      </ItemThumbnailStyled>
+    </Link>
   );
 };
